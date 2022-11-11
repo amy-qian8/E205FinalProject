f7468ce3a10472ecf6fcba5c8147787237b0cdd7
Amy Qian <amyq88@gmail.com>
2022-11-11 10:42:28 -0800

    added stationary csv

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ for file in os.listdir():
 print('Done loading data.')
 
 # Compile all the accel data into one array, rename the column titles
-accel_data_all.columns = ["Stationary Kaanthi", "Walking Kaanthi"]
+accel_data_all.columns = ["Stationary Kaanthi", "Stationary Sidney", "Stationary Amy", "Walking Kaanthi"]
 print(accel_data_all)
 
 def createPDFHistogram():
 
     # Plot the histogram for Stationary Kaanthi
     # i = range(len(accel_data_all.columns))
-    i = 1
+    i = [0,1,2]
     energyArr = np.array(accel_data_all.iloc[:, i])
     print(energyArr)
     plt.hist(energyArr)
     # Plotting the PDF (probability density function) to the histogram
     mu, std = norm.fit(energyArr)
     x = np.linspace(min(energyArr), max(energyArr), 100)
-    pdf = norm.pdf(x, mu, std) * 10000  # note big scale factor
+    pdf = norm.pdf(x, mu, std) * 5000  # note big scale factor
     plt.plot(x, pdf, 'k', linewidth=2)
     name = list(accel_data_all.columns)
     plt.suptitle("Histogram of " + name[i], fontsize=12)
