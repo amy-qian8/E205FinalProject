52176377f5894c9352c7acc01295fb25658fd8f4
Amy Qian <amyq88@gmail.com>
2022-11-11 11:03:34 -0800

    all files

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ for file in os.listdir():
 print('Done loading data.')
 
 # Compile all the accel data into one array, rename the column titles
-accel_data_all.columns = ["Stationary Kaanthi", "Stationary Sidney", "Stationary Amy", "Walking Kaanthi"]
+accel_data_all.columns = ["Stationary Kaanthi", "Stationary Sidney", "Stationary Amy", "Walking Kaanthi", "Walking Sidney", "Walking Amy", "Running Kaanthi", "Running Sidney", "Running Amy"]
 print(accel_data_all)
 
 def createPDFHistogram():
@@ -26,19 +26,24 @@ def createPDFHistogram():
     # i = range(len(accel_data_all.columns))
     i = [0,1,2]
     energyArr = np.array(accel_data_all.iloc[:, i])
-    print(energyArr)
-    plt.hist(energyArr)
+    combinedEnergyArr = np.array(accel_data_all.iloc[:, i[0]])
+    combinedEnergyArr = np.append(combinedEnergyArr, np.array(accel_data_all.iloc[:, i[1]]), axis=0)
+    combinedEnergyArr = np.append(combinedEnergyArr, np.array(accel_data_all.iloc[:, i[2]]), axis=0)
+    print(np.array(accel_data_all.iloc[:, i[2]]))
+    print(len(np.array(accel_data_all.iloc[:, i[2]])))
+
+    plt.hist(combinedEnergyArr)
     # Plotting the PDF (probability density function) to the histogram
-    mu, std = norm.fit(energyArr)
-    x = np.linspace(min(energyArr), max(energyArr), 100)
+    mu, std = norm.fit(combinedEnergyArr)
+    x = np.linspace(min(combinedEnergyArr), max(combinedEnergyArr), 100)
     pdf = norm.pdf(x, mu, std) * 5000  # note big scale factor
     plt.plot(x, pdf, 'k', linewidth=2)
     name = list(accel_data_all.columns)
-    plt.suptitle("Histogram of " + name[i], fontsize=12)
+    plt.suptitle("Histogram of " + "Stationary Data", fontsize=12)
     plt.title("p(ei|xi = stationary)", fontsize=10)
     plt.xlabel("Energy [m/s]")
     plt.ylabel("Count of data points")
     plt.show()
-    return mu, std, energyArr
+    return mu, std, combinedEnergyArr
 
 createPDFHistogram()
